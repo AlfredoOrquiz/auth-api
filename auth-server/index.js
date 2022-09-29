7ee028d12ddc5340ec431c7aeea8b691ae5c066e
Alfredo <fredy_rios0123@outlook.com>
2022-09-29 05:43:19 +0000

    Lab 08

--- auth-server/index.js
+++ auth-server/index.js
@@ -3,7 +3,10 @@
 require('dotenv').config();
 const app = require('./src/server.js');
 const { db } = require('./src/auth/models');
+const { db } = require('./src/models');
+const server = require('./src/server.js');
 
 db.sync().then(() => {
+  server.start(3000);
   app.start(process.env.PORT || 3001);
 });
--- auth-server/src/auth/models/index.js
+++ auth-server/src/auth/models/index.js
@@ -1,13 +1,22 @@
 'use strict';
 
+const { Sequelize, DataTypes } = require('sequelize');
+const clothesModel = require('../auth/models/clothes/model.js');
+const foodModel = require('../auth/models/food/model.js');
+const Collection = require('../auth/models/data-collection.js');
+
 const userModel = require('./users.js');
 const { Sequelize, DataTypes } = require('sequelize');
 
 const DATABASE_URL = process.env.DATABASE_URL || 'sqlite:memory;';
 
 const sequelize = new Sequelize(DATABASE_URL);
+const food = foodModel(sequelize, DataTypes);
+const clothes = clothesModel(sequelize, DataTypes);
 
 module.exports = {
   db: sequelize,
+  food: new Collection(food),
+  clothes: new Collection(clothes),
   users: userModel(sequelize, DataTypes),
 }
